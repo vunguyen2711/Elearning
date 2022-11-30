b97a4850105509aba731a9519881737185ba3a67
macOS <macos@adsl-172-14-1-150.dsl.lgtpmi.sbcglobal.net>
2022-11-30 09:41:18 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+window.addEventListener("load",function(){
+    truncateCardTitle();
+});
+
+function truncateCardTitle(){
+    var cardList = document.getElementsByClassName("card-title");
+    console.log(cardList);
+    for(var i = 0; i < cardList.length; i++){
+        var text = cardList[i].innerHTML;
+        var newText = truncateString(text,35);
+        cardList[i].innerHTML = newText;
+    }
+}
+
+function truncateString(str,num){
+    if(str.length > num){
+        return str.slice(0,num) + "...";
+    } else{
+        return str;
+    }
+}
